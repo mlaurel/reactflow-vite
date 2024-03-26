a3aa49832c3d608d76b78d4c91c7af48cf569750
Mike Laurel <mlaurel@hey.com>
2024-03-25 18:07:27 -0700

    Adds Pardot locator flow example

--- src/edges/index.ts
+++ src/edges/index.ts
@@ -1,11 +1,14 @@
 import type { Edge, EdgeTypes } from "reactflow";
 
 export const initialEdges = [
-  { id: "a->c", source: "a", target: "c", animated: true },
-  { id: "b->d", source: "b", target: "d" },
-  { id: "c->d", source: "c", target: "d", animated: true },
+    { id: "a->c", source: "a", target: "c", animated: true },
+    { id: "a->b", source: "a", target: "b", animated: true },
+    { id: "b->d", source: "b", target: "d", animated: true },
+    { id: "c->d", source: "c", target: "d", animated: true },
+    { id: "c->e", source: "c", target: "e", animated: true },
+    { id: "d->e", source: "d", target: "e", animated: true },
 ] satisfies Edge[];
 
 export const edgeTypes = {
-  // Add your custom edge types here!
+    // Add your custom edge types here!
 } satisfies EdgeTypes;
--- src/nodes/index.ts
+++ src/nodes/index.ts
@@ -2,23 +2,38 @@ import type { Node, NodeTypes } from "reactflow";
 import { PositionLoggerNode } from "./PositionLoggerNode";
 
 export const initialNodes = [
-  { id: "a", type: "input", position: { x: 0, y: 0 }, data: { label: "wire" } },
-  {
-    id: "b",
-    type: "position-logger",
-    position: { x: -100, y: 100 },
-    data: { label: "drag me!" },
-  },
-  { id: "c", position: { x: 100, y: 100 }, data: { label: "your ideas" } },
-  {
-    id: "d",
-    type: "output",
-    position: { x: 0, y: 200 },
-    data: { label: "with React Flow" },
-  },
+    {
+        id: "a",
+        type: "input",
+        position: { x: 0, y: 0 },
+        data: { label: "Pardot Form Handler" }
+    },
+    {
+        id: "b",
+        type: "output, position-logger",
+        position: { x: -150, y: 110 },
+        data: { label: "User Submits Form" },
+    },
+    {
+        id: "c",
+        position: { x: 100, y: 100 },
+        data: { label: "Xata.io Database Capture" }
+    },
+    {
+        id: "d",
+        type: "output",
+        position: { x: 0, y: 200 },
+        data: { label: "Mapbox Geolocation" },
+    },
+    {
+        id: "e",
+        type: "output, input, position-logger",
+        position: { x: 200, y: 200 },
+        data: { label: "Render User on Dealer Locator" },
+    }
 ] satisfies Node[];
 
 export const nodeTypes = {
-  "position-logger": PositionLoggerNode,
-  // Add any of your custom nodes here!
+    "position-logger": PositionLoggerNode,
+    // Add any of your custom nodes here!
 } satisfies NodeTypes;
